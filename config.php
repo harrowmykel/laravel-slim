86e03f66fe127594e5d85c19ea3be8c033fd7b80
harrowmykel <harrowwestmykel@gmail.com>
2022-10-19 14:58:26 +0200

    first push

--- config.php
+++ config.php
@@ -5,6 +5,25 @@ namespace APP;
 defined('APP_READY') || die();
 
 
- class Config{
+class Config{
 
+    //instance fof the Config class
+    protected static $instance;
+
+    public static function i(): Config
+    {
+        if (self::$instance == null) {
+            self::$instance = new Config();
+            self::$instance->init();
+        }
+        return self::$instance;
+    }
+
+
+    //define constants here
+    public function init()
+    {
+    	//this should be the link to your website 
+    	define('ENVIRONMENT_URL', 'https://example.com');
+    }
 }
